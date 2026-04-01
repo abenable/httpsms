170b297d433de82e5d05bddf2add21620010c171
able <abenable@outlook.com>
2026-04-01 21:37:12 +0300

    Update Firebase hosting and Android build settings
    - Add top-level Android Gradle buildscript with Google services
      classpath
    - Remove the old com.google.gms.google-services plugin declaration
    - Update Firebase project to http-sms-1738 in web/.firebaserc
    - Update web/package.json deploy and preview scripts to use
      http-sms-1738
    - Add web hosting cache file at web/.firebase/hosting.ZGlzdA.cache

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,7 +1,17 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("com.google.gms:google-services:4.4.2")
+    }
+}
+
 plugins {
     id("com.android.application") version "9.1.0" apply false
     id("com.android.library") version "9.1.0" apply false
-    id("com.google.gms.google-services") version "4.4.4" apply false
 }
 
 tasks.register<Delete>("clean") {
